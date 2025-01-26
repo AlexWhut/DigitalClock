ed031de3505cdfc1f3f47871a4735eedb39e32aa
AlexWhut <bryanmoranteswt@gmail.com>
2025-01-26 18:18:58 +0100

    V1

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import DigitalClock from "./DigitalClock/DigitalClock";
+
+ReactDOM.render(<DigitalClock />, document.getElementById("root"));
